4bdcbfb0e6b64d7d31f6b4f49d22efc6142d0c47
chaos11600 <azaankayani963@gmail.com>
2026-05-16 14:47:20 +0100

    chaosiscumin

--- extension/darkmode/main.js
+++ extension/darkmode/main.js
@@ -1,55 +1,49 @@
 const DarkMode = (() => {
 
-    let _settings = {};
+    let settings = {};
 
-    const CSS_ID = "darkmodeSP";
-
-    function setCSS(path) {
-        const el = document.getElementById("darkmodeSP") 
-            || Object.assign(document.createElement("link"), {
-                id: "darkmodeSP",
-                rel: "stylesheet"
-            });
-
-        document.head.appendChild(el);
-
-        el.href = new URL(path, chrome.runtime.getURL("darkmode/")).toString();
+    function loadCSS(file) {
+        let link = document.getElementById("bsparx-dark");
+        if (!link) {
+            link = document.createElement("link");
+            link.id = "bsparx-dark";
+            link.rel = "stylesheet";
+            document.head.appendChild(link);
+        }
+        link.href = chrome.runtime.getURL("darkmode/" + file);
     }
 
     function removeEffects() {
-        document.querySelectorAll(".SparxPlusImageInverted")
-            .forEach(el => el.classList.remove("SparxPlusImageInverted"));
+        document.querySelectorAll(".SparxPlusImageInverted").forEach(el => {
+            el.classList.remove("SparxPlusImageInverted");
+        });
     }
 
     return {
 
-        init(settings) {
-            _settings = settings || {};
+        init(s) {
+            settings = s || {};
         },
 
         applyDarkMode() {
-
-            setCSS("darkmode/darkmaths.css");
-
-          document.body.style.cssText += "background: #0a0a0a !important; background-image: none !important;";
+            loadCSS("darkmaths.css");
+            document.body.style.cssText += "background: #0a0a0a !important; background-image: none !important;";
 
             document.querySelectorAll("img, video").forEach(el => {
-                if (_settings.darkModeImages) {
+                if (settings.darkModeImages) {
                     el.style.filter = "invert(1) hue-rotate(180deg)";
                 }
             });
 
             document.querySelectorAll("canvas").forEach(c => {
-                c.style.backgroundColor =
-                    _settings.whiteboardDarkModeOverride ? "white" : "#2a2a28";
+                c.style.backgroundColor = settings.whiteboardDarkModeOverride ? "white" : "#0a0a0a";
             });
         },
 
         revokeDarkMode() {
-
-            setCSS("darkmode/defaultmaths.css");
-
+            loadCSS("defaultmaths.css");
             document.body.style.background = "";
+            document.body.style.backgroundImage = "";
 
             document.querySelectorAll("img, video").forEach(el => {
                 el.style.filter = "";
@@ -65,18 +59,8 @@ const DarkMode = (() => {
 
 })();
 
-// message bridge
-chrome.runtime.onMessage.addListener((msg) => {
-
-    if (msg.settings) {
-        DarkMode.init(msg.settings);
-    }
-
-    if (msg.action === "applyDarkMode") {
-        DarkMode.applyDarkMode();
-    }
-
-    if (msg.action === "revokeDarkMode") {
-        DarkMode.revokeDarkMode();
-    }
+chrome.runtime.onMessage.addListener(msg => {
+    if (msg.settings) DarkMode.init(msg.settings);
+    if (msg.action === "applyDarkMode") DarkMode.applyDarkMode();
+    if (msg.action === "revokeDarkMode") DarkMode.revokeDarkMode();
 });
